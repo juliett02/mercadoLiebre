a8c043bd3564022d0bede1a169b91c498d7c3850
juliett02 <rinaudojulieta@gmail.com>
2021-05-07 11:58:20 -0300

    entrega mercado liebre

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { dirname } = require('path');
+const app = express();
+const path = require("path");
+
+let pathHtml = path.resolve(__dirname, "./public");
+
+app.use(express.static(pathHtml));
+
+
+app.get('/', (req, res) => {    
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get('/form-registro', function(req , res){
+    let htmlPath = path.join(__dirname,'./views/form-registro.html' );
+    res.sendFile(htmlPath);
+});
+
+app.get('/login', function(req , res){
+    let htmlPath = path.join(__dirname,'./views/login.html' );
+    res.sendFile(htmlPath);
+});
+
+
+app.listen('3000', function () {
+    console.log('Estoy corriendo en el puerto 3000');
+})
+
+
+
+
+
+
